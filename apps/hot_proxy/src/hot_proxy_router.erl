992196c178ae0fffc9f6a1adf4b5c2abc067a78e
Sebastian Green-Husted <ricecake@tfm.nu>
2015-11-07 14:58:22 -0500

    Update hot_proxy_router.erl

--- apps/hot_proxy/src/hot_proxy_router.erl
+++ apps/hot_proxy/src/hot_proxy_router.erl
@@ -27,7 +27,7 @@
 %%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 %%% (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 %%% OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--module(toy_router).
+-module(hot_proxy_router).
 -behaviour(vegur_interface).
 -export([init/2,
          terminate/3,

6665b56b365c62934e624a25ade9be9e7b59d7ad
Sebastian Green-Husted <ricecake@tfm.nu>
2015-11-19 18:51:48 -0500

    add an interface module stub

---
+++ apps/hot_proxy/src/hot_proxy.erl
@@ -0,0 +1,13 @@
+-module(hot_proxy).
+-export([
+	list_domains/0,
+	get_domain/1,
+	insert_domain/3,
+	remove_domain/1,
+	insert_alias/2,
+	remove_alias/2,
+	insert_host/2,
+	remove_host/2,
+	update_host/2
+
+]).

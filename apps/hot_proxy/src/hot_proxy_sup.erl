af2597fd3982a63b5b6fdd40421416dc7793d312
Sebastian Green-Husted <ricecake@tfm.nu>
2015-11-27 14:48:05 -0500

    initialize event tables, and start server

--- apps/hot_proxy/src/hot_proxy_sup.erl
+++ apps/hot_proxy/src/hot_proxy_sup.erl
@@ -32,9 +32,11 @@ start_link() ->
 init([]) ->
 	ok = hot_proxy_route_table:init_tables(),
 	ok = hot_proxy_config:init_tables(),
+	ok = hot_proxy_event:init_tables(),
 	{ok, { {one_for_all, 0, 1}, [
+		?CHILD(hot_proxy_route_table, worker),
 		?CHILD(hot_proxy_config, worker),
-		?CHILD(hot_proxy_route_table, worker)
+		?CHILD(hot_proxy_event, worker)
 	]} }.
 
 %%====================================================================

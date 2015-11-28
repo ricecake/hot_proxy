5e85aa455f3cf38e81eee951d05e696566fd3444
Sebastian Green-Husted <ricecake@tfm.nu>
2015-11-28 17:59:32 -0500

    simplify table startup

--- apps/hot_proxy/src/hot_proxy_sup.erl
+++ apps/hot_proxy/src/hot_proxy_sup.erl
@@ -30,9 +30,8 @@ start_link() ->
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
 init([]) ->
-	ok = hot_proxy_route_table:init_tables(),
-	ok = hot_proxy_config:init_tables(),
-	ok = hot_proxy_event:init_tables(),
+	Modules = [hot_proxy_route_table, hot_proxy_config, hot_proxy_event],
+	[ ok = Module:init_tables() || Module <- Modules],
 	{ok, { {one_for_all, 0, 1}, [
 		?CHILD(hot_proxy_route_table, worker),
 		?CHILD(hot_proxy_config, worker),
